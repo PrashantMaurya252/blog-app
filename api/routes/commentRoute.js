00dfd0a1af96b5f39a0aaa432594b70e24ac53c7
PrashantMaurya252 <mauryaprashant252@gmail.com>
2024-02-03 21:41:42 +0530

    delete comment functionality added

--- api/routes/commentRoute.js
+++ api/routes/commentRoute.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { createComment, getPostComments, likeComment } from '../controllers/commentController.js';
+import { createComment, deleteComment, editComment, getPostComments, likeComment } from '../controllers/commentController.js';
 import { getUser } from '../controllers/userController.js';
 
 const router=express.Router();
 
 router.post('/create',verifyToken,createComment);
 router.get('/getPostComments/:postId',getPostComments);
-router.put('/likeComment/:commentId',verifyToken,likeComment)
+router.put('/likeComment/:commentId',verifyToken,likeComment);
+router.put('/editComment/:commentId',verifyToken,editComment);
+router.delete('/deleteComment/:commentId',verifyToken,deleteComment);
+
 
 
 
--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,16 +1,18 @@
-import { Alert, Button, Textarea } from "flowbite-react";
+import { Alert, Button, Modal, Textarea } from "flowbite-react";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { Link,useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Comment from "./Comment";
+import { HiOutlineExclamationCircle } from "react-icons/hi";
 
 function CommentSection({ postId }) {
   const { currentUser } = useSelector((state) => state.user);
   const [comment, setComment] = useState("");
   const [commentError, setCommentError] = useState(null);
   const [comments, setComments] = useState([]);
-  const navigate=useNavigate()
-  
+  const [showModal,setShowModal]=useState(false);
+  const [commentToDelete,setCommentToDelete]=useState(null)
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -49,14 +51,14 @@ function CommentSection({ postId }) {
           const data = await res.json();
 
           setComments(data);
-        } 
+        }
         // else if (res.status !== 200) {
-          
+
         //   const data = await res.json();
         //   console.log("Multiple data:", data);
-          
+
         //   setComments(data);
-        // } 
+        // }
         // else {
         //   console.log("Server response not okay:", res);
         // }
@@ -68,27 +70,60 @@ function CommentSection({ postId }) {
   }, [postId]);
 
   const handleLike = async (commentId) => {
-    console.log(commentId)
+    console.log(commentId);
     try {
-        if(!currentUser){
-            navigate('sign-in');
-            return
-        }
-        const res=await fetch(`/api/comment/likeComment/${commentId}`,{
-            method:'PUT'
-        });
-        if(res.ok){
-            const data=await res.json()
-            setComments((comments)=>comments.map((comment)=>
-                comment._id === commentId ?{...comment,likes:data.likes,numberOfLikes:data.likes.length}:comment
-            ))
-        }else{
-            console.log('Something went wrong')
-        }
+      if (!currentUser) {
+        navigate("sign-in");
+        return;
+      }
+      const res = await fetch(`/api/comment/likeComment/${commentId}`, {
+        method: "PUT",
+      });
+      if (res.ok) {
+        const data = await res.json();
+        setComments((comments) =>
+          comments.map((comment) =>
+            comment._id === commentId
+              ? {
+                  ...comment,
+                  likes: data.likes,
+                  numberOfLikes: data.likes.length,
+                }
+              : comment
+          )
+        );
+      } else {
+        console.log("Something went wrong");
+      }
     } catch (error) {
-        console.log(error.message)
+      console.log(error.message);
     }
   };
+  const handleEdit = async (comment, editedContent) => {
+    setComments(comments.map((c) => c._id === comment._id ? {...c,content:editedContent}:c));
+  };
+  const handleDelete=async(commentId)=>{
+    setShowModal(false)
+    try {
+      if(!currentUser){
+        navigate('/sign-in');
+        return
+      }
+      const res = await fetch(`/api/comment/deleteComment/${commentId}`,{
+        method:'DELETE',
+      });
+      if(res.ok){
+        const data=await res.json();
+            setComments(
+              comments.filter((comment)=>comment._id !== commentId)
+            )
+          
+        
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
     <div className="max-w-2xl mx-auto w-full p-3">
       {currentUser ? (
@@ -151,10 +186,26 @@ function CommentSection({ postId }) {
             </div>
           </div>
           {comments.map((comment) => (
-            <Comment key={comment._id} comment={comment} onLike={handleLike} />
+            <Comment key={comment._id} comment={comment} onLike={handleLike} onEdit={handleEdit} onDelete={(commentId)=>{
+              setShowModal(true);
+              setCommentToDelete(commentId)
+            }}/>
           ))}
         </>
       )}
+      <Modal show={showModal} onClose={()=>setShowModal(false)} popup size="md">
+                <Modal.Header/>
+                <Modal.Body>
+                    <div className="text-center">
+                        < HiOutlineExclamationCircle  className="h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto"/>
+                        <h3 className="mb-5 text-lg text-gray-500 dark:text-gray-400">Are You sure you want to delete this Comment ?</h3>
+                        <div className="flex justify-center gap-4">
+                            <Button color="failure" onClick={()=>handleDelete(commentToDelete)}>Yes,I am Sure</Button>
+                            <Button color="gray" onClick={()=>setShowModal(false)}>Cancel</Button>
+                        </div>
+                    </div>
+                </Modal.Body>
+            </Modal>
     </div>
   );
 }
