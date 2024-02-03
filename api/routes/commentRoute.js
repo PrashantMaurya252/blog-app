cf1b0cbbbeee84f019363dc4d5daaeda6e619a6e
PrashantMaurya252 <mauryaprashant252@gmail.com>
2024-02-03 12:23:00 +0530

    added loke functionalty to comment

--- api/routes/commentRoute.js
+++ api/routes/commentRoute.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { createComment, getPostComments } from '../controllers/commentController.js';
+import { createComment, getPostComments, likeComment } from '../controllers/commentController.js';
 import { getUser } from '../controllers/userController.js';
 
 const router=express.Router();
 
 router.post('/create',verifyToken,createComment);
 router.get('/getPostComments/:postId',getPostComments);
+router.put('/likeComment/:commentId',verifyToken,likeComment)
 
 
 
--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,107 +1,162 @@
 import { Alert, Button, Textarea } from "flowbite-react";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
 import Comment from "./Comment";
 
-function CommentSection({postId}) {
-    const {currentUser}=useSelector(state=>state.user);
-    const [comment,setComment]=useState('');
-    const [commentError,setCommentError]=useState(null);
-    const [comments,setComments]=useState([]);
-    console.log(comments)
-    
-    const handleSubmit=async(e)=>{
-        e.preventDefault();
-        if(comment.length >200){
+function CommentSection({ postId }) {
+  const { currentUser } = useSelector((state) => state.user);
+  const [comment, setComment] = useState("");
+  const [commentError, setCommentError] = useState(null);
+  const [comments, setComments] = useState([]);
+  const navigate=useNavigate()
+  
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (comment.length > 200) {
+      return;
+    }
+    try {
+      const res = await fetch("/api/comment/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          content: comment,
+          postId,
+          userId: currentUser._id,
+        }),
+      });
+      const data = await res.json();
+      if (res.ok) {
+        setComment("");
+        setCommentError(null);
+        setComments([data, ...comments]);
+      }
+    } catch (error) {
+      setCommentError(error.message);
+    }
+  };
+
+  useEffect(() => {
+    const getComments = async () => {
+      try {
+        const res = await fetch(`/api/comment/getPostComments/${postId}`);
+
+        if (res.ok) {
+          const data = await res.json();
+
+          setComments(data);
+        } 
+        // else if (res.status !== 200) {
+          
+        //   const data = await res.json();
+        //   console.log("Multiple data:", data);
+          
+        //   setComments(data);
+        // } 
+        // else {
+        //   console.log("Server response not okay:", res);
+        // }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    getComments();
+  }, [postId]);
+
+  const handleLike = async (commentId) => {
+    console.log(commentId)
+    try {
+        if(!currentUser){
+            navigate('sign-in');
             return
         }
-        try {
-            const res = await fetch('/api/comment/create',{
-                method:'POST',
-                headers:{
-                    'Content-Type':'application/json'
-                },
-                body:JSON.stringify({content:comment,postId,userId:currentUser._id}),
-            });
-            const data=await res.json();
-            if(res.ok){
-                setComment('')
-                setCommentError(null);
-                setComments([data,...comments])
-                
-            }
-        } catch (error) {
-            setCommentError(error.message)
+        const res=await fetch(`/api/comment/likeComment/${commentId}`,{
+            method:'PUT'
+        });
+        if(res.ok){
+            const data=await res.json()
+            setComments((comments)=>comments.map((comment)=>
+                comment._id === commentId ?{...comment,likes:data.likes,numberOfLikes:data.likes.length}:comment
+            ))
+        }else{
+            console.log('Something went wrong')
         }
-        
+    } catch (error) {
+        console.log(error.message)
     }
-
-    useEffect(()=>{
-        const getComments=async()=>{
-            try {
-                const res=await fetch(`/api/comment/getPostComments/${postId}`);
-                if(res.ok){
-                    const data=await res.json();
-                    setComments(data)
-                    
-                }else if (res.status === 300) {
-                    // Handle multiple choices - you can choose one or display choices to the user
-                    const choices = await res.json();
-                    console.log("Multiple choices:", choices);
-                    // Depending on your app logic, you might want to prompt the user to choose one
-                    setComments(choices)
-                  }
-                else {
-                    console.log("Server response not okay:", res);
-                  }
-            } catch (error) {
-                console.log(error.message)
-            }
-        };
-        getComments()
-    },[postId])
-    return (
-        <div className="max-w-2xl mx-auto w-full p-3">
-            {currentUser ? (
-                <div className="flex items-center gap-1 my-5 text-gray-500 text-sm">
-                    <p>Signed in as:</p>
-                    <img className="h-5 w-5 object-cover rounded-full" src={currentUser.profilePicture} />
-                    <Link to='/dashboard?tab=profile' className="text-xs text-cyan-600 hover:underline">
-                       @{currentUser.username}
-                    </Link>
-                </div>
-            ):(
-                <div className="text-sm text-teal-500 my-5 flex gap-1">
-                    You must be Log in to comment
-                    <Link className="text-blue-500 hover:underline" to='/sign-in'>Sign in</Link>
-                </div>
-            )}
-            {currentUser && (
-                <form onSubmit={handleSubmit} className="border border-teal-500 rounded-md p-3">
-                    <Textarea onChange={(e)=>setComment(e.target.value)} value={comment} placeholder="Add a comment..." row="3" maxLength='200'/>
-                    <div className="flex justify-between items-center mt-5">
-                        <p className="text-gray-500 text-xs">{200-comment.length} characters remaining</p>
-                        <Button outline gradientDuoTone="purpleToBlue" type="submit">Submit</Button>
-                    </div>
-                    {commentError && (
-                        <Alert color="failure" className="mt-5">{commentError}</Alert>
-                    )}
-                </form>
-            )}
-            {comments.length ===0 ? (
-                <p className="text-sm my-5">No comments yet !</p>
-            ):(
-                <>
-                  <div className="text-sm my-5 flex items-center gap-1">
-                    <p>Comments</p>
-                    <div className="border border-gray-400 py-1 px-2 rounded-sm">{comments.length}</div>
-                  </div>
-                  {comments.map((comment)=>(<Comment key={comment._id} comment={comment}/>))}
-                </>
-            )}
+  };
+  return (
+    <div className="max-w-2xl mx-auto w-full p-3">
+      {currentUser ? (
+        <div className="flex items-center gap-1 my-5 text-gray-500 text-sm">
+          <p>Signed in as:</p>
+          <img
+            className="h-5 w-5 object-cover rounded-full"
+            src={currentUser.profilePicture}
+          />
+          <Link
+            to="/dashboard?tab=profile"
+            className="text-xs text-cyan-600 hover:underline"
+          >
+            @{currentUser.username}
+          </Link>
         </div>
-    )
+      ) : (
+        <div className="text-sm text-teal-500 my-5 flex gap-1">
+          You must be Log in to comment
+          <Link className="text-blue-500 hover:underline" to="/sign-in">
+            Sign in
+          </Link>
+        </div>
+      )}
+      {currentUser && (
+        <form
+          onSubmit={handleSubmit}
+          className="border border-teal-500 rounded-md p-3"
+        >
+          <Textarea
+            onChange={(e) => setComment(e.target.value)}
+            value={comment}
+            placeholder="Add a comment..."
+            row="3"
+            maxLength="200"
+          />
+          <div className="flex justify-between items-center mt-5">
+            <p className="text-gray-500 text-xs">
+              {200 - comment.length} characters remaining
+            </p>
+            <Button outline gradientDuoTone="purpleToBlue" type="submit">
+              Submit
+            </Button>
+          </div>
+          {commentError && (
+            <Alert color="failure" className="mt-5">
+              {commentError}
+            </Alert>
+          )}
+        </form>
+      )}
+      {comments.length === 0 ? (
+        <p className="text-sm my-5">No comments yet !</p>
+      ) : (
+        <>
+          <div className="text-sm my-5 flex items-center gap-1">
+            <p>Comments</p>
+            <div className="border border-gray-400 py-1 px-2 rounded-sm">
+              {comments.length}
+            </div>
+          </div>
+          {comments.map((comment) => (
+            <Comment key={comment._id} comment={comment} onLike={handleLike} />
+          ))}
+        </>
+      )}
+    </div>
+  );
 }
 
-export default CommentSection
+export default CommentSection;
