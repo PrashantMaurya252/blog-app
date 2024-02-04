4583a9a298788ede5a696a9590f3e64757b3510b
PrashantMaurya252 <mauryaprashant252@gmail.com>
2024-02-04 23:57:21 +0530

    Home page modification

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,50 @@
+import { Link } from "react-router-dom";
+import CallToAction from "../components/CallToAction";
+import { useEffect, useState } from "react";
+import PostCard from "../components/PostCard";
 function Home() {
-    return (
-        <div>
-            Home
-        </div>
-    )
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch(`/api/post/getposts`);
+      const data = await res.json();
+      setPosts(data.posts);
+    };
+    fetchPosts();
+  }, []);
+  return (
+    <div>
+      <div className="flex flex-col gap-6 px-3 p-28 max-w-6xl mx-auto">
+        <h1 className="text-3xl font-bold lg:text-6xl">Welcome To My Blog</h1>
+        <p className="text-gray-500 text-xs sm:text-sm">
+          Here you will find various type of Blog
+        </p>
+        <Link
+          to="/search"
+          className="text-xs sm:text-sm text-teal-500 font-bold hover:underline"
+        >
+          View All Posts
+        </Link>
+      </div>
+      <div className="p-3 bg-amber-100 dark:bg-slate-700">
+        <CallToAction />
+      </div>
+      <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7">
+        {posts && posts.length > 0 && (
+          <div className="flex flex-col gap-6">
+            <h2 className="text-2xl font-semibold text-center">Recent Posts</h2>
+            <div className="flex flex-wrap gap-4">
+              {posts.map((post) => (
+                <PostCard key={post._id} post={post}/>
+              ))}
+            </div>
+            <Link to={'/search'} className="text-lg text-teal-500 hover:underline text-center">View All Posts</Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
