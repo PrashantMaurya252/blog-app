42d0c6ed92351b846a8cc87293d93a75763c9355
PrashantMaurya252 <mauryaprashant252@gmail.com>
2024-02-05 12:46:25 +0530

    about and projects pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,42 @@
 function About() {
-    return (
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
         <div>
-            About
+          <h1 className="text-3xl font-semibold text-center my-7">
+            About Prashant's Blog
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Welcome to Prashant's Blog – your coding companion in the digital
+              realm! Run by Prashant, a passionate programmer, this blog is your
+              one-stop destination for diverse programming insights. From
+              beginner-friendly tutorials to advanced tips, Prashant Blog caters
+              to all skill levels. Stay updated on the latest tech trends,
+              explore various programming languages, and join a vibrant
+              community of fellow enthusiasts. Embark on a coding journey where
+              innovation meets education at Prashant Blog!
+            </p>
+
+            <p>
+              Discover the coding universe with Prashant's Blog – a hub curated by
+              Prashant, offering insightful programming content. From
+              novice-friendly guides to advanced insights, stay tuned for the
+              latest tech trends and engage with a dynamic coding community.
+              Join us at Prashant Blog for an innovative and educational coding
+              experience!
+            </p>
+            <p>
+              We value your thoughts! Share feedback on Prashant Blog—your
+              insights help us improve. Leave comments on posts for a
+              collaborative learning experience. Thank you for being part of our
+              community!
+            </p>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default About
+export default About;
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,9 +1,19 @@
+import CallToAction from "../components/CallToAction";
+
 function Projects() {
-    return (
-        <div>
-            Projects
-        </div>
-    )
+  return (
+    <div className="min-h-scrren max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3">
+      <h1 className="text-3xl font-semibold">Projects</h1>
+      <p className="text-md text-gray-500">
+        Explore Prashant's Blog's projects – a showcase of innovative coding
+        endeavors. From web development to data science, each project represents
+        a creative exploration in the world of programming. Dive into these
+        concise, impactful creations and get inspired for your coding
+        adventures!
+      </p>
+      <CallToAction />
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
